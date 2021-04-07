7249061397e7a109c41c35f1dce04c98d7b354e2
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-04-07 15:36:25 +0200

    corrected exercise

--- exercice_1/main.js
+++ exercice_1/main.js
@@ -1,3 +1,6 @@
 function applyBorder() {
   document.getElementById('image').style.border = 'thick solid #ff0000';
 }
+function noBorder() {
+  document.getElementById('image').style.border ='none';
+}
