bb5aace8679db8d95353e0603be2ae77effee659
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-04-08 14:52:48 +0200

    corrected exercises

---
+++ exercice_3/main.js
@@ -0,0 +1,14 @@
+function displayChangeTextColor(id,color) {
+  document.getElementById('text').style.color = color;
+}
+/*
+function displayRedText() {
+  document.getElementById('text').style.color = '#ff0000';
+}
+function displayBlueText() {
+  document.getElementById('text').style.color = '#0000ff';
+}
+function displayGreenText() {
+  document.getElementById('text').style.color = '#008000';
+}
+*/
